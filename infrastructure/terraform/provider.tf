5f9a47ae98d4892d0f311233a573fc82b871d118
isaacmorrison <89813336+isaacmorrison@users.noreply.github.com>
2022-06-03 06:45:53 -0700

    Update provider.tf

--- infrastructure/terraform/provider.tf
+++ infrastructure/terraform/provider.tf
@@ -16,4 +16,4 @@ provider "azurerm" {
 }
 
 # Make client_id, tenant_id, subscription_id and object_id variables
-data "azurerm_client_config" "current" {}
+# data "azurerm_client_config" "current" {}
